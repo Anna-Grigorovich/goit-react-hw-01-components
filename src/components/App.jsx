f9c1571623cdad6711a46fc1638671e809da06f2
Anna Grigorovich <115174523+Anna-Grigorovich@users.noreply.github.com>
2023-05-01 16:55:56 +0300

    дз

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Profile } from "./Profile/Profile";
+import user from '../user';
+import { Statistics } from "./Statistics/Statistics";
+import statistics from '../data'
+import { FriendList } from "./FriendList/FriendList";
+import friends from '../friends'
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile userProfile = {user}/>
+      <Statistics title="Upload stats" stats={statistics} />
+      < FriendList friends={friends}/>
+    </>
   );
 };
